aa2c291049df7e013a7d5d84b77c046d7c401257
akashdesaimasai <akash.desai3105@gail.com>
2023-11-23 22:43:15 +0530

    url

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
 const URL = process.env.MONGOOSE_URL;
-const connection = mongoose.connect(URL);
+const connection = mongoose.connect('mongodb+srv://akashdesai3105:akash3105@cluster0.mksequa.mongodb.net/Eval4Async');
 if(connection){
     console.log("connected");
 }
