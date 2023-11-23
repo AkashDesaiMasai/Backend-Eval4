790de487ccc9775c6bbe6abb37e2e7c2cc85fc85
akashdesaimasai <akash.desai3105@gail.com>
2023-11-23 22:39:07 +0530

    package cors updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 const PORT = process.env.PORT||8000;
-const cors = require("cors")
+const cors = require("cors");
 const { Authentication } = require("./middlewares");
 
 const { connection } = require("./db");
